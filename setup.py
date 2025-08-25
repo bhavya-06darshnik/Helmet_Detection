55e56d8eb3ec593adc28eb57ebd519651427a505
Bhavya <bhavyaky0690@gmail.com>
2025-08-25 09:40:45 +0530

    All pipelines and setup done.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="helmet",
+    version="0.0.1",
+    author="Bhavya",
+    author_email="bhavyaky0690gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
